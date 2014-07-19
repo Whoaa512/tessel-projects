a6e9e3c8117b64626dc62e7b24c31b6e24fd3d16
C. James Winslow <whoaa512@gmail.com>
2014-07-19 14:48:02 -0700

    sorta working. pushing for brian

--- amazingserver.js
+++ amazingserver.js
@@ -6,40 +6,84 @@ if it can connect:
   if it's ok, light up green lights
 */
 
-var subprocess = require('child_process');
+var once = require('once')
+var wifi = require('wifi-cc3000');
 var led = require('tessel-led');
-var tessel = require('tessel-client');
-var env = process.env;
 var timer = require('./timer.js');
 
+var wifiSettings = {
+  ssid: 'Hack Reactor',
+  password: 'awesomebullets',
+  // security: 'wpa2',
+  // timeout: 30
+}
+, connectionCheckIntervalId = null;
 
 var startCheckingConnectionSpeed = function() {
-  setInterval(function() {
+  connectionCheckIntervalId = setInterval(function() {
     if (timer.connectionSpeedSucks()) {
+      console.log('connection speed sucks!')
       // turn both on
       led.red.show();
       led.green.show();
-    }
-    else {
+    } else {
+      console.log('connection speed is good!')
       led.red.hide()
       led.green.show();
     }
   }, 4000);
 };
 
+var startConnectionCheck = once(function(err, res) {
+  console.log('startConnectionCheck called')
+  if (err) {
+    led.green.hide();
+    led.red.show();
+    console.log('ERROR:', err);
+  } else {
+    startCheckingConnectionSpeed()
+  }
+});
+
+function tryConnect() {
+  if (wifi.isConnected()) {
+    startConnectionCheck(null, wifi.connection())
+  } else {
+    if (!wifi.isBusy()) {
+      console.log("not busy");
+      console.log('trying to connect')
+      wifi.connect(wifiSettings, startConnectionCheck);
+    } else {
+      console.log("is busy, trying again");
+      setTimeout(function() {
+        tryConnect();
+      }, 1000);
+    }
+  }
+}
+
+wifi.on('connect', function(error) {
+  console.log('wifi connected!')
+  if (error) console.log('Wifi connection error:', error)
+  // start looping to check connection
+  startConnectionCheck(null, wifi.connection())
+});
+
+wifi.on('disconnect', function(error) {
+  console.log('wifi disconnected!')
+  if (error) console.log('Wifi disconnection error:', error)
+  // kill loop
+  clearInterval(connectionCheckIntervalId);
+  // try to connect again
+  // tryConnect()
+});
 
 console.log('loading stuff');
-led.green.flash(3, 100, 100, function(){
-  var client = tessel.connect(8080, 'localhost');
-  client.configureWifi('Hack Reactor', 'awesomebullets', 'wpa2', opts, function(err, status){
-    if (stderr) {
-      // solid red if it's just not working at all
-      led.green.hide();
-      led.red.show();
-    }
-    else {
-      // check connection speed in a loop
-      startCheckingConnectionSpeed();
-    }
-  });
-});
+setTimeout(function() {
+  if (!wifi.isConnected()) {
+    tryConnect()
+  } else if (wifi.isConnected()) {
+    startConnectionCheck(null, wifi.connection())
+  }
+}, 10000)
+
