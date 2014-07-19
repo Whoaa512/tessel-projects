5a6b25fed6d17ef0d79d17843c7a3ad962c53b8b
Brian Weidenbaum <brianweiden@gmail.com>
2014-07-19 11:40:59 -0700

    if the request takes too long anyway, run the cb immediately rather than waiting indefinitely for it to finish

--- timer.js
+++ timer.js
@@ -8,10 +8,10 @@ var getElapsedSeconds = function(now, old) {
 /**
  * Makes an http request, and returns whether it took too long
 */
-var requestLengthSucks function(maxLength) {
+var requestLengthSucks function(maxSeconds) {
   maxLength = maxLength || 2;
   var startTime = new Date();
-  makeRequest(function(result){
+  makeRequest(maxSeconds, function(result){
     if (result.error) {
       console.log(result.error);
       return true;
@@ -19,18 +19,29 @@ var requestLengthSucks function(maxLength) {
     else {
       var elapsedSeconds = getElapsedSeconds(result.now, startTime);
       console.log(elapsed);
-      return elapsedSeconds > maxLength;
+      return elapsedSeconds > maxSeconds;
     }
   });
 };
 
-var makeRequest = function(cb) {
+var makeRequest = function(maxSeconds, cb) {
+  var done = false;
+
+  // if we are waiting longer than the maxSeconds anyway, run the cb
+  setTimeout(function(){
+    if (!done) {
+      cb({now: new Date(), err: null});
+    }
+  }, maxSeconds*1000 + 1);
+
   http.get('http://httpstat.us/200', function(res){
     res.on('end', function(){
+      done = true;
       cb({now: new Date(), err: null});
     });
   })
   .on('error', function(err){
+    done = true;
     cb({now: new Date(), err: err});
   });
 };
